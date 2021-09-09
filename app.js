9fbc084362ed325876972adedc5648c3dd79da2e
Sudhan Bhattarai <bhattaraiavinav400@gmail.com>
2021-09-09 20:17:22 +0545

    changed require to import

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
-const express = require('express');
+//Importing express framework and bodyparser to make requests
+import express from 'express';
+import bodyParser from 'body-parser';
+
+// Making express call and port 
 const app =express();
 const PORT= 5000;
+
+// Body of Rest Api
+app.use(bodyParser.json()); // we will be using json data
 
 app.listen(PORT,()=>{
     console.log(`Listening on server ${PORT}`);
