47db725c57ed960c9544e86f3093a8d123f93c5f
Sudhan Bhattarai <bhattaraiavinav400@gmail.com>
2021-09-12 19:08:12 +0545

    Added a rendering and many things

--- app.js
+++ app.js
@@ -1,19 +1,41 @@
-//Importing express framework and bodyparser to make requests
+//Importing modules
 import express from 'express';
 import bodyParser from 'body-parser';
+import dotenv from 'dotenv'
 import userRoutes from './Routes/users.js';
 import productRoutes from './Routes/products.js';
+import morgan from 'morgan'
+import path from 'path'
+const __dirname = path.resolve();
+
 // Making express call and port 
 const app =express();
-const PORT= 5000;
+dotenv.config({path:'.env'});
+const PORT= process.env.PORT || 5000;
 
-// Body of Rest Api
+//Requests
+app.use(morgan('tiny'));
+app.use(bodyParser.urlencoded({extended : true}))
 app.use(bodyParser.json()); // we will be using json data
+
+//Setting View Engine
+app.set('view engine','ejs');
+
+
+// Loading Static Folders
+app.use('/css',express.static(path.resolve(__dirname,"assets/css")))
+app.use('/images',express.static(path.resolve(__dirname,"assets/images")))
+app.use('/js',express.static(path.resolve(__dirname,"assets/js")))
+
+//Routing
 app.use('/users',userRoutes);
 app.use('/products',productRoutes);
 
-app.get('/',(req,res)=>{res.send("Hello World!!!");})
+//Get Requests
+app.get('/',(req,res)=>{res.render('index');})
 
+
+//Listening to server
 app.listen(PORT,()=>{
-    console.log(`Listening on server ${PORT}`);
+    console.log(`Listening on server http://localhost:${PORT}`);
 })
